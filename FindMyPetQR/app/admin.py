b0e473ab29cbb67fd93c24937f2cba23d58db1f0
Claudio Cortes <claudio.cortes@vasslatam.com>
2023-10-05 15:05:57 -0300

    Cambios en modelo de datos

--- FindMyPetQR/app/admin.py
+++ FindMyPetQR/app/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import TipoMascota
+from .models import TipoMascota, RazaMascota, UserProfile, Mascota
 
 # Register your models here.
 
+class RazaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'tipo_mascota']
+    search_fields = ['nombre']
+    list_filter = ['tipo_mascota']
+
+class MascotasAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'tipo', 'raza', 'dueño']
+    search_fields = ['nombre', 'dueño']
+    list_filter = ['tipo']
+
 admin.site.register(TipoMascota)
+admin.site.register(RazaMascota, RazaAdmin)
+admin.site.register(UserProfile)
+admin.site.register(Mascota, MascotasAdmin)
--- FindMyPetQR/app/models.py
+++ FindMyPetQR/app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class TipoMascota(models.Model):
@@ -8,4 +8,34 @@ class TipoMascota(models.Model):
     def __str__(self):
         return self.nombre
     
+class RazaMascota(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo_mascota = models.ForeignKey(TipoMascota, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.nombre
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    direccion = models.CharField(max_length=100, null=True)
+    ciudad = models.CharField(max_length=50, null=True)
+    imagen = models.ImageField(upload_to="perfil", null=True)
+    
+
+    def __str__(self):
+        return self.user.username
+
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.ForeignKey(TipoMascota, on_delete=models.CASCADE)
+    raza = models.ForeignKey(RazaMascota, on_delete=models.CASCADE)
+    edad = models.PositiveIntegerField()
+    dueño = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    imagen = models.ImageField(upload_to="mascotas", null=True)
+    descripcion = models.TextField(max_length=200, null=True)
+    detalles = models.TextField(max_length=200, null=True)
+    
+
+    def __str__(self):
+        return self.nombre
