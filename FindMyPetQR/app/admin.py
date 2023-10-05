04dfdff6a6e2a477444fe5f45c9f260175bf3e35
clcortesp <82979072+clcortesp@users.noreply.github.com>
2023-10-05 11:59:10 -0300

    Add files via upload

---
+++ FindMyPetQR/app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import TipoMascota
+
+# Register your models here.
+
+admin.site.register(TipoMascota)
---
+++ FindMyPetQR/app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+#from django.contrib.auth.models import User
+
+# Create your models here.
+class TipoMascota(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+    
+
