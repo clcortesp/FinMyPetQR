1ff723d1c6c4df996419fd51ff359a9e39e9cf63
Claudio Cortes <claudio.cortes@vasslatam.com>
2023-10-27 03:56:11 -0300

    Nueva app inv

--- FindMyPetQR/FindMyPetQR/settings.py
+++ FindMyPetQR/FindMyPetQR/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'inv',
     'rest_framework',
 ]
 
---
+++ FindMyPetQR/inv/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path, include
+from app.views import home, landing, registro, profile, petProfile, newPet, RazaViewset, MascotasViewset, deletePet, petEdit, reportar_perdida, reportar_encontrada, profileEdit
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'apiRazas', RazaViewset, basename='raza')
+router.register(r'apiMascotas', MascotasViewset)
+
+
+urlpatterns = [
+    path('', landing, name="home"),
+
+    path('registro/', registro, name='registro'),
+    path('api/', include(router.urls)),
+
+    path('profile/', profile, name="profile"),
+    path('profileEdit/', profileEdit, name="profileEdit"),
+    path('newPet/', newPet, name="newPet"),
+    path('petProfile/<id>/', petProfile, name="petProfile"),
+    path('deletePet/<id>/', deletePet, name="deletePet"),
+    path('petEdit/<id>/', petEdit, name="petEdit"),
+
+    path('reportar_perdida/<int:id>/', reportar_perdida, name='reportar_perdida'),
+    path('reportar_encontrada/<int:id>/', reportar_encontrada, name='reportar_encontrada'),
+
+]
+
+urlpatterns += router.urls
