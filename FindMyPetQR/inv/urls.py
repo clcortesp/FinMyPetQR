eaa6a3a502de5f7edb5e6dbe8acb68bf2b11dfe1
Claudio Cortes <claudio.cortes@vasslatam.com>
2023-10-27 04:23:18 -0300

    mas cambios

--- FindMyPetQR/inv/urls.py
+++ FindMyPetQR/inv/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from app.views import home, landing, registro, profile, petProfile, newPet, RazaViewset, MascotasViewset, deletePet, petEdit, reportar_perdida, reportar_encontrada, profileEdit
+from app.views import landing, registro, profile, petProfile, newPet, RazaViewset, MascotasViewset, deletePet, petEdit, reportar_perdida, reportar_encontrada, profileEdit
 from rest_framework import routers
+from .views import home
 
 router = routers.DefaultRouter()
 router.register(r'apiRazas', RazaViewset, basename='raza')
@@ -8,20 +9,20 @@ router.register(r'apiMascotas', MascotasViewset)
 
 
 urlpatterns = [
-    path('', landing, name="home"),
+    path('casa/', home, name="home"),
 
-    path('registro/', registro, name='registro'),
+    #path('registro/', registro, name='registro'),
     path('api/', include(router.urls)),
 
-    path('profile/', profile, name="profile"),
-    path('profileEdit/', profileEdit, name="profileEdit"),
-    path('newPet/', newPet, name="newPet"),
-    path('petProfile/<id>/', petProfile, name="petProfile"),
-    path('deletePet/<id>/', deletePet, name="deletePet"),
-    path('petEdit/<id>/', petEdit, name="petEdit"),
+    #path('profile/', profile, name="profile"),
+    #path('profileEdit/', profileEdit, name="profileEdit"),
+    #path('newPet/', newPet, name="newPet"),
+    #path('petProfile/<id>/', petProfile, name="petProfile"),
+    #path('deletePet/<id>/', deletePet, name="deletePet"),
+    #path('petEdit/<id>/', petEdit, name="petEdit"),
 
-    path('reportar_perdida/<int:id>/', reportar_perdida, name='reportar_perdida'),
-    path('reportar_encontrada/<int:id>/', reportar_encontrada, name='reportar_encontrada'),
+    #path('reportar_perdida/<int:id>/', reportar_perdida, name='reportar_perdida'),
+    #path('reportar_encontrada/<int:id>/', reportar_encontrada, name='reportar_encontrada'),
 
 ]
 
--- FindMyPetQR/inv/views.py
+++ FindMyPetQR/inv/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from rest_framework import viewsets
+from app.serializers import RazaSerializer, MascotasSerializer
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from app.models import Mascota, UserProfile, RazaMascota, TipoMascota, ServicioApi
+from app.forms import UserRegistrationForm, MascotaForm, MascotaFormEdit, ProfileFormEdit
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from app.api import getToken, enviar_correo, getDireccion
 
 # Create your views here.
+def home(request):
+    return render(request, 'inv/base.html')
